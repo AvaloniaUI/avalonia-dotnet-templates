84546537ffd17f0a3af0985a6b8de68990465b27
Steven Kirk <grokys@gmail.com>
2020-12-17 12:58:06 +0100

    Enable nullable reference types.

--- templates/app-mvvm/ViewLocator.cs
+++ templates/app-mvvm/ViewLocator.cs
@@ -1,6 +1,3 @@
-// Copyright (c) The Avalonia Project. All rights reserved.
-// Licensed under the MIT license. See licence.md file in the project root for full license information.
-
 using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
@@ -14,12 +11,12 @@ namespace AvaloniaAppTemplate
 
         public IControl Build(object data)
         {
-            var name = data.GetType().FullName.Replace("ViewModel", "View");
+            var name = data.GetType().FullName!.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
             if (type != null)
             {
-                return (Control)Activator.CreateInstance(type);
+                return (Control)Activator.CreateInstance(type)!;
             }
             else
             {
