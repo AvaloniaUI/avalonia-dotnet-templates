c241065ce8fd9d878ad56ce15991bbf8ec6fa2d3
GMIKE <roman.soloweow333@ya.ru>
2021-02-04 03:13:45 +0300

    Example work with validation and IObservable

--- templates/csharp/app-reactiveui/ViewModels/MainWindowViewModel.cs
+++ templates/csharp/app-reactiveui/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
+using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Abstractions;
+using ReactiveUI.Validation.Contexts;
+using ReactiveUI.Validation.Extensions;
+using Splat;
 
 namespace AvaloniaAppTemplate.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase, IValidatableViewModel
     {
-        [Reactive] public string Greeting { get; set; } = "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            this.ValidationRule(
+                viewModel => viewModel.Name,
+                name => !string.IsNullOrWhiteSpace(name),
+                "You must specify a valid name");
+            
+            GreetingMessage = this.WhenAnyValue(x => x.Name).Select(name => $"{name}, Welcome to Avalonia!");
+        }
+        [Reactive] public string Name { get; set; } = string.Empty;
+        public IObservable<string> GreetingMessage { get; }
+
+        public ValidationContext ValidationContext { get; } = new ValidationContext();
     }
 }
--- templates/csharp/app-reactiveui/Views/MainWindow.axaml.cs
+++ templates/csharp/app-reactiveui/Views/MainWindow.axaml.cs
@@ -1,10 +1,12 @@
 using System.Reactive.Disposables;
+using System.Threading;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using AvaloniaAppTemplate.ViewModels;
 using ReactiveUI;
+using ReactiveUI.Validation.Extensions;
 
 namespace AvaloniaAppTemplate.Views
 {
@@ -20,12 +22,32 @@ namespace AvaloniaAppTemplate.Views
 //+:cnd:noEmit
         }
         
-        public TextBlock GreetingTextBlock => this.FindControl<TextBlock>("GreetingTextBlock");
+        private TextBlock Greeting => this.FindControl<TextBlock>("Greeting");
+        private TextBlock NameValidation => this.FindControl<TextBlock>("NameValidation");
+        private TextBox Name => this.FindControl<TextBox>("Name");
+
         private void InitializeComponent()
         {
             this.WhenActivated(disposables =>
             {
-                this.OneWayBind(this.ViewModel, x => x.Greeting, x=>x.GreetingTextBlock.Text).DisposeWith(disposables);
+                this.Bind(ViewModel, 
+                        x=>x.Name, 
+                        x=>x.Name.Text)
+                    .DisposeWith(disposables);
+                
+                this.BindValidation(ViewModel, 
+                        x => x.Name, 
+                            x => x.NameValidation.Text)
+                    .DisposeWith(disposables);
+
+                this.WhenAnyObservable(x => x.ViewModel.GreetingMessage)
+                    .BindTo(this, x => x.Greeting.Text)
+                    .DisposeWith(disposables);
+
+                this.Bind(ViewModel, 
+                        x=>x.ValidationContext.IsValid, 
+                        x=>x.Greeting.IsVisible)
+                    .DisposeWith(disposables);
             });
 
             AvaloniaXamlLoader.Load(this);
