b13eb3e3c072dcc63dc22c839950fbb6754aa7e3
Dan Walmsley <dan@walms.co.uk>
2022-03-08 22:48:41 +0000

    add android.

---
+++ templates/csharp/xplat/AvaloniaTest.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia.Android;
+using Avalonia;
+
+namespace AvaloniaTest.Android
+{
+    [Activity(Label = "AvaloniaTest.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class MainActivity : AvaloniaActivity<App>
+    {
+        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder);
+        }
+    }
+}
