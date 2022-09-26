c95da8601e66e747ad85a13d80125b08be1fdd00
Takoooooo <tako0qq@gmail.com>
2022-09-26 16:49:33 +0300

    Test

--- templates/csharp/app-mvvm/Program.cs
+++ templates/csharp/app-mvvm/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Avalonia;
-using Avalonia.ReactiveUI;
 using System;
 
 namespace AvaloniaAppTemplate
@@ -17,7 +16,6 @@ namespace AvaloniaAppTemplate
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToTrace()
-                .UseReactiveUI();
+                .LogToTrace();
     }
 }
