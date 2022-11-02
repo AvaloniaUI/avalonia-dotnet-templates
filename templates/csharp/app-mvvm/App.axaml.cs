963e7ee51e77d96920bff47bbf2f459dccf9e98f
Daniil Pavliuchyk <tako0qq@gmail.com>
2022-11-02 15:49:50 +0200

    Add comment

--- templates/csharp/app-mvvm/App.axaml.cs
+++ templates/csharp/app-mvvm/App.axaml.cs
@@ -19,6 +19,8 @@ namespace AvaloniaAppTemplate
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                // Line below is needed to remove Avalonia data validation.
+                // Without this line you will get duplicate validations from both Avalonia and CT
                 ExpressionObserver.DataValidators.RemoveAll(x => x is DataAnnotationsValidationPlugin);
                 desktop.MainWindow = new MainWindow
                 {
