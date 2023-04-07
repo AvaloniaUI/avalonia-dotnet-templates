9f7339633c940292a502a392803d9adf2731ccc6
mrxx99 <mrxx99@googlemail.com>
2023-04-08 00:13:56 +0200

    Make MainWindowViewModel partial when used with CommunityToolkit.Mvvm

--- templates/csharp/app-mvvm/ViewModels/MainWindowViewModel.cs
+++ templates/csharp/app-mvvm/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace AvaloniaAppTemplate.ViewModels;
 
+#if (CommunityToolkitChosen)
+public partial class MainWindowViewModel : ViewModelBase
+#else
 public class MainWindowViewModel : ViewModelBase
+#endif
 {
     public string Greeting => "Welcome to Avalonia!";
 }
