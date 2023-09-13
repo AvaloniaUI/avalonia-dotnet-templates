5e4335615aa08e35c342f3b7abe26084c96b54cf
mrxx99 <mrxx99@googlemail.com>
2023-09-13 21:42:51 +0200

    fix nullability warnings and unify viewlocator

--- templates/fsharp/xplat/AvaloniaTest/ViewLocator.fs
+++ templates/fsharp/xplat/AvaloniaTest/ViewLocator.fs
@@ -9,11 +9,14 @@ type ViewLocator() =
     interface IDataTemplate with
         
         member this.Build(data) =
-            let name = data.GetType().FullName.Replace("ViewModel", "View")
-            let typ = Type.GetType(name)
-            if isNull typ then
-                upcast TextBlock(Text = sprintf "Not Found: %s" name)
-            else
-                downcast Activator.CreateInstance(typ)
+            if isNull data then
+                null
+            else    
+                let name = data.GetType().FullName.Replace("ViewModel", "View", StringComparison.Ordinal)
+                let typ = Type.GetType(name)
+                if isNull typ then
+                    upcast TextBlock(Text = sprintf "Not Found: %s" name)
+                else
+                    downcast Activator.CreateInstance(typ)
 
         member this.Match(data) = data :? ViewModelBase
