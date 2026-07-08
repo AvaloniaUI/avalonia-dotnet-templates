be5ae1b18a4eac9e538f81be1df900f3cbad777d
Max Katz <maxkatz6@outlook.com>
2026-07-08 01:25:11 -0700

    Use two way bindings for XPlat tempalte too

--- templates/csharp/xplat/AvaloniaTest/ViewModels/SettingsViewModel.cs
+++ templates/csharp/xplat/AvaloniaTest/ViewModels/SettingsViewModel.cs
@@ -1,19 +1,26 @@
-﻿#if (CommunityToolkitChosen)
+#if (CommunityToolkitChosen)
 using CommunityToolkit.Mvvm.ComponentModel;
+#elif (ReactiveUIToolkitChosen)
+using ReactiveUI;
 #endif
 
 namespace AvaloniaTest.ViewModels;
 
 #if (CommunityToolkitChosen)
 public partial class SettingsViewModel : ViewModelBase
-#else
-public class SettingsViewModel : ViewModelBase
-#endif
 {
-#if (CommunityToolkitChosen)
     [ObservableProperty]
-    private string _greeting = "This is Settings";
-#else
-    public string Greeting { get; } = "This is Settings";
-#endif
+    public partial string Greeting { get; set; } = "This is Settings";
 }
+#elif (ReactiveUIToolkitChosen)
+public class SettingsViewModel : ViewModelBase
+{
+    private string _greeting = "This is Settings";
+
+    public string Greeting
+    {
+        get => _greeting;
+        set => this.RaiseAndSetIfChanged(ref _greeting, value);
+    }
+}
+#endif
