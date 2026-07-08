8f7dac0a8f503abf8aa02c625eeb9431722873c9
Max Katz <maxkatz6@outlook.com>
2026-07-07 23:47:58 -0700

    Use two way bindings in MVVM template

--- templates/csharp/app-mvvm/ViewModels/MainViewModel.cs
+++ templates/csharp/app-mvvm/ViewModels/MainViewModel.cs
@@ -1,10 +1,30 @@
-﻿namespace AvaloniaAppTemplate.ViewModels;
+#if (CommunityToolkitChosen)
+using CommunityToolkit.Mvvm.ComponentModel;
+#elif (ReactiveUIToolkitChosen)
+using ReactiveUI;
+#endif
+
+namespace AvaloniaAppTemplate.ViewModels;
 
 #if (CommunityToolkitChosen)
 public partial class MainViewModel : ViewModelBase
-#else
-public class MainViewModel : ViewModelBase
-#endif
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+#if (UsePartialProperties)
+    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+#else
+    private string _greeting = "Welcome to Avalonia!";
+#endif
 }
+#elif (ReactiveUIToolkitChosen)
+public class MainViewModel : ViewModelBase
+{
+    private string _greeting = "Welcome to Avalonia!";
+
+    public string Greeting
+    {
+        get => _greeting;
+        set => this.RaiseAndSetIfChanged(ref _greeting, value);
+    }
+}
+#endif
